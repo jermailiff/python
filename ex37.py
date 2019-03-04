16678ab16dc7418079af63d61da0ea675374badf
Jermailiff <40833162+jermailiff@users.noreply.github.com>
2019-03-04 20:49:09 +0000

    First commit

---
+++ ex37.py
@@ -0,0 +1,83 @@
+import os
+# from math import sqrt
+# 
+# 
+# print "Hello World"
+# 
+# while True:
+# 
+# 	feelings = input("How are you feeling this morning on a scale of 1 - 10?")
+# 	
+# 	if feelings in range(1,5):
+# 		print "Pretty shitty then!"
+# 		break
+# 	elif feelings in range(6,10):
+# 		print "Pretty damn good eh"
+# 		break
+# 	else:
+# 		print "You're confusing as fuck"
+# 
+# secondary = input("How did you feel yesterday on that same scale?")
+# try:
+# 	val = int(secondary)
+# except ValueError:
+# 	print "[You stooopid, clearly that means you need to enter a number!]"
+# 
+# 
+# if feelings % 2 == 0 and secondary % 2 == 0:
+# 	print feelings % 2 == 0 and secondary % 2 == 0
+# 	print "You've been feeling pretty even of late pal."
+# else:
+# 	print "You odd bastard."
+# 
+# print max(sqrt(feelings), sqrt(secondary))
+# 
+# print "Your secondary feeling isn't important as today is a new day, so let's fuck it off."
+# 
+# del secondary
+# 
+# if feelings%2 == 0 or sqrt(feelings) > 1.5:
+# 	print True
+# else:
+# 	print "I'm just being random"
+# 
+# def location():
+# 	loaction = raw_input("What's your current location trooper? ")
+# 	pass
+# 	return location
+# 
+# location()
+# 
+# locations = [location, 2, 'uk', 'germany', 4]
+# 
+# print "What are the odds you pick a location?"
+# pick = input("Choose a number? ")
+# 
+# if pick != int(pick):
+# 	raise TypeError("Only intergers accepted")
+# 
+# # resolve
+# print location[pick] 
+# 
+
+
+
+# with
+# assert
+#  yield
+# class
+# exec
+# finally 
+
+os.chdir(r 'C:\users\jermaine\documents')
+
+enter = open('test1.rtf')
+for line in enter:
+	print line
+close()
+	
+
+
+
+
+
